ea250a475a77ed08ec38812d9f9affe7d90e9871
hidra1001 <hidra1001@gmail.com>
2026-09-13 23:06:11 +0900

    무지개 놀이터 앱 배포

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 무지개 놀이터 서비스 워커: 오프라인에서도 놀 수 있게 파일을 저장해요 */
-const VERSION = 'rainbow-202609132302';
+const VERSION = 'rainbow-202609132306';
 const CORE = ['./', './index.html', './math.html', './hangul.html', './brain.html', './arcade.html', './voice.js', './layout.css', './layout.js', './fit.js', './audio/list.json', './qr.png', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
